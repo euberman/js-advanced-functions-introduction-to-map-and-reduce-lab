ef5f9a9e87cbbece8b329dfb446283475d85c36e
euberman <euberman@gmail.com>
2020-11-23 15:42:54 -0600

    commit tree 25c51132d67b60d6d242ed8991b3487d4f0fe629

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+function mapToNegativize(sourceArray){
+  return sourceArray.map(item => (-1) *item);
+};
+
+function mapToNoChange(sourceArray){
+  return sourceArray.map(item => item);
+};
+
+function mapToDouble(sourceArray){
+  return sourceArray.map(item => item * 2);
+};
+
+function mapToSquare(sourceArray){
+  return sourceArray.map(item => item ** 2);
+};
+// ================================
+function reduceToTotal(sourceArray, startingPoint=0){
+  return sourceArray.reduce((total, item) => total + item, startingPoint);
+};
+
+function reduceToAllTrue(sourceArray){
+  return sourceArray.reduce((total, item) => !item, 0);
+};
+
+function reduceToAnyTrue(sourceArray){
+  return sourceArray.reduce((total, item) => item, 0);
+};
